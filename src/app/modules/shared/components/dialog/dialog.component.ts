1c88d123eaba67c7f596cef5493a533a0109b880
Admin <sahil.samkria@mobilecoderz.com>
2022-08-01 19:35:16 +0530

    third commit

--- src/app/modules/shared/components/dialog/dialog.component.ts
+++ src/app/modules/shared/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -7,11 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
+    
+  images : any = [];
 
-  constructor() { }
+  TodoTasksForm = this.fb.group({
+    images:[''],
+    title: [''],
+    description: [''],
+    date:[''],
+    status : ['']
+  });
+  constructor(
+    private fb: FormBuilder 
+  ) { }
 
   ngOnInit(): void {
   }
- 
-
+   
+  addTasks(){
+   console.log(this.TodoTasksForm.value) 
+  }
+  openImagePopUp() {    
+    document.getElementById('uploadImageUrl')?.click();
+  }
+   
+  // getImages(event : any){
+  //   console.log(event);
+  //       if (event.target.files && event.target.files[0]) {
+  //           var reader = new FileReader();
+  //           reader.onload = (event: any) => {
+  //               this.images = event.target.result;
+  //           }
+  //           reader.readAsDataURL(event.target.files[0]);
+  //       }
+  // }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule } from '@angular/core';
+import {CommonModule } from '@angular/common';
 
-import { SharedRoutingModule } from './shared-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {SharedRoutingModule } from './shared-routing.module';
+import {HeaderComponent } from './components/header/header.component';
+import {ReactiveFormsModule } from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-// import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDialogModule } from '@angular/material/dialog';
-import { DialogComponent } from './components/dialog/dialog.component';
+import {DialogComponent } from './components/dialog/dialog.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
-import { UserListComponent } from './components/user-list/user-list.component'
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatSortModule} from '@angular/material/sort';
+import {UserListComponent } from './components/user-list/user-list.component';
+import {MatTableModule} from '@angular/material/table';
 
 
 
@@ -35,20 +35,21 @@ import {MatSortModule} from '@angular/material/sort';
     MatToolbarModule,
     MatIconModule,
     MatInputModule ,
-    MatButtonModule,
-    // MatCardModule,
+    MatButtonModule,    
     MatFormFieldModule,
     MatDialogModule,
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule ,
     MatPaginatorModule ,
-    MatSortModule 
+    MatSortModule, 
+    MatTableModule
   ],
   
   exports:[
     HeaderComponent,
-    DialogComponent
+    DialogComponent,
+    UserListComponent
   ]
 })
 export class SharedModule { }
